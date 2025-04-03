12582576183a26b3494e1b2ffdaa4020402a3fd5
FathimaNajlak <fathnajlahabeeb@gmail.com>
2025-04-03 12:49:48 +0530

    Added Dice App project with rolling functionality and UI improvements

--- projects/dice_app/lib/dice_page.dart
+++ projects/dice_app/lib/dice_page.dart
@@ -14,26 +14,36 @@ class _DicePageState extends State<DicePage> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Row(
+      child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           TextButton(
-              style: TextButton.styleFrom(),
-              onPressed: () {
-                setState(() {
-                  print('$diceNum  pressed');
-                  diceNum = Random().nextInt(6) + 1;
-                });
-              },
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(26),
-                child: Image.asset(
-                  'assets/images/dice$diceNum.jpg',
-                  height: 150,
-                  width: 150,
-                  fit: BoxFit.cover,
-                ),
-              ))
+            style: TextButton.styleFrom(),
+            onPressed: () {
+              setState(() {
+                print('$diceNum  pressed');
+                diceNum = Random().nextInt(6) + 1;
+              });
+            },
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(26),
+              child: Image.asset(
+                'assets/images/dice$diceNum.jpg',
+                height: 150,
+                width: 150,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          Text(
+            'You got $diceNum',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w500,
+              color: Colors.white,
+            ),
+          ),
         ],
       ),
     );
--- projects/dice_app/lib/main.dart
+++ projects/dice_app/lib/main.dart
@@ -3,10 +3,16 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: Scaffold(
       backgroundColor: Colors.green[900],
       appBar: AppBar(
+        title: Text(
+          'Roll the Dice',
+          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+        ),
         backgroundColor: Colors.green[900],
+        centerTitle: true,
       ),
       body: DicePage(),
     ),
